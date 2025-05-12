72ac06c81800538c4ea340c72eaa8dbb5048207c
mzkki <08harismu@gmail.com>
2025-05-12 23:03:24 +0800

    feat: add Swagger UI integration and API documentation

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,12 @@ import v1Routes from './routes/v1';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 import { errorHandler, notFoundHandler } from './middlewares/errorHandler';
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../api-spec.yml'));
 
 const app = express();
 
@@ -12,6 +17,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/v1', v1Routes);
 
